b4544054e52f689a2cf4f8eee8590f17e44a0773
EnCelad8 <naxmax70@gmail.com>
2023-06-20 20:04:39 +0300

    Implemented function printGameTable

--- src/game_noughts_and_crosses.java
+++ src/game_noughts_and_crosses.java
@@ -70,7 +70,14 @@ public class game_noughts_and_crosses {
     }
 
     private static void printGameTable(char[][] gameTable) {
-
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
     }
 
 
